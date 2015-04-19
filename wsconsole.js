6443e8b755eef48763280f618a459b14c585c5d1
bucho <steelwind666@gmail.com>
2015-04-20 06:32:55 +0900

    リファクタリング modified:   wsconsole.js

--- wsconsole.js
+++ wsconsole.js
@@ -1,28 +1,17 @@
 // console screen class
-function ConsoleScreen(height, id) {
-  var screen = document.createElement('div');
-  this._id = id;
-  screen.id = this._id;
-  document.body.appendChild(screen);
-  for (var i = 0; i < height; i++) {
-    var line = document.createElement('div');
-    line.innerHTML = '<br>';
-    screen.appendChild(line);
-  }
-  this.backBuffer = screen.cloneNode(true);
-
-  this.write = function(line, message) {
+function Screen(height, id) {
+  Screen.prototype.write = function(line, message) {
     lines = this.backBuffer.getElementsByTagName('div');
     lines[line].innerHTML = message;
   };
 
-  this.flip = function() {
+  Screen.prototype.flip = function() {
     var front = document.getElementById(this._id);
     document.body.replaceChild(this.backBuffer, front);
     this.backBuffer = this.backBuffer.cloneNode(true);
   };
 
-  this.receve = function(data) {
+  Screen.prototype.receve = function(data) {
     var commands, i, length;
     commands = data.split('\x00');
     length = commands.length;
@@ -31,10 +20,25 @@ function ConsoleScreen(height, id) {
     }
     this.flip();
   }
+
+  Screen.prototype._create_div = function(text) {
+    var div = document.createElement('div');
+    div.innerHTML = text;
+    return div;
+  }
+
+  var screen = document.createElement('div');
+  this._id = id;
+  screen.id = this._id;
+  document.body.appendChild(screen);
+  for (var i = 0; i < height; i++) {
+    screen.appendChild(this._create_div('<br>'));
+  }
+  this.backBuffer = screen.cloneNode(true);
 }
 
 function MapScreen(height, id) {
-  var that = new ConsoleScreen(height, id);
+  var that = new Screen(height, id);
 
   that._accept = function(command) {
     var match, line, message;
@@ -48,13 +52,11 @@ function MapScreen(height, id) {
 }
 
 function MessageScreen(height, id) {
-  var that = new ConsoleScreen(height, id);
+  var that = new Screen(height, id);
 
   that.add = function(message) {
     this.backBuffer.removeChild(this.backBuffer.firstChild);
-    var line = document.createElement('div');
-    line.innerHTML = message;
-    this.backBuffer.appendChild(line);
+    this.backBuffer.appendChild(this._create_div(message));
     this.flip();
   }
 
