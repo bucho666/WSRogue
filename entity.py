e5d6e36f054e94ed5c52bf1f82b70062230a0580
bucho <steelwind666@gmail.com>
2015-05-10 08:56:28 +0900

    移動できない地形を実装 modified:   entity.py modified:   game.py

--- entity.py
+++ entity.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 class Entity(object):
   def __init__(self, glyph, color, name):
-    self._glyph = glyph
-    self._color = color
+    self._graphic = Graphic(glyph, color)
     self._name = name
 
   def render(self, screen, coordinate):
-    screen.put(self._glyph, coordinate, self._color)
+    self._graphic.render(screen, coordinate)
 
   def name(self):
     return self._name
+
+class Graphic(object):
+  def __init__(self, glyph, color):
+    self._glyph = glyph
+    self._color = color
+
+  def render(self, screen, coordinate):
+    screen.put(self._glyph, coordinate, self._color)
--- game.py
+++ game.py
@@ -37,13 +37,17 @@ class Map(object):
     return None
 
   def is_open(self, coordinate):
-    return not coordinate in self._character
+    if coordinate in self._character: return False
+    return self.walkable(coordinate)
+
+  def walkable(self, (x, y)):
+    return self._cell[y][x].walkable()
 
   def random_open_coordinate(self):
     opens = []
     w, h = self.size()
-    for y in range(w):
-      for x in range(h):
+    for y in range(h):
+      for x in range(w):
         if self.is_open((x, y)): opens.append((x, y))
     return random.choice(opens)
 
@@ -58,12 +62,22 @@ class Terrain(Character):
   def __init__(self, glyph, color, name='地形'):
     Entity.__init__(self, glyph, color, name)
 
+  def walkable(self):
+    return True
+
+class Wall(Character):
+  def __init__(self, glyph, color, name='壁'):
+    Entity.__init__(self, glyph, color, name)
+
+  def walkable(self):
+    return False
+
 class MapFile(object):
   _terrain = {
     '.': Terrain('.', 'silver', '床'),
-    '#': Terrain('#', 'silver', '壁'),
-    'T': Terrain('T', 'green',  '木'),
-    '~': Terrain('~', 'blue' ,  '湖'),
+    '#': Wall('#', 'silver', '壁'),
+    'T': Wall('T', 'green',  '木'),
+    '~': Wall('~', 'blue' ,  '湖'),
     '=': Terrain('=', 'olive' , '橋'),
   }
 
